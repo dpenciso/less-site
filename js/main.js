a5a03fbfde5cd27c287f7c013e00bdab9e8c309c
David Enciso <dpeniciso@msn.com>
2020-03-19 13:22:42 -0600

    finished desk and mobile nav

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function openNav() {
+    document.querySelector("#open").style.display = "none";
+    document.querySelector("#nav").style.width = "100%";
+}
+function closeNav() {
+    document.querySelector("#nav").style.width = "0";
+    document.querySelector("#open").style.display = "block";
+}
